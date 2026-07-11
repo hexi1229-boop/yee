66ae6775aff0388b1e15440751cd292a0ff575c5
yangchao02 <yangchao02@qiyi.com>
2026-07-11 17:57:03 +0800

    Deploy visual storybook

---
+++ _next/static/WhAaM185Va5zNOUAPBJYw/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/yee//_next/:path+",
+        "destination": "/yee/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
